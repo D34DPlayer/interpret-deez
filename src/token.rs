baeb0b0066d980c41539bd4c90a4e118b0f60899
Carlos Ruiz Herrera <carlos.ruiz.herrera@rock.estate>
2023-06-07 19:47:49 +0200

    (feat) Tokens

---
+++ src/token.rs
@@ -0,0 +1,37 @@
+#[derive(Debug, PartialEq)]
+pub enum Token<'a> {
+    // Deprecated
+    EOF, // None used instead
+    //Illegal(char), // Unicode support so everything allowed
+    // Identifiers + literals
+    Ident(&'a str),
+    Int(&'a str),
+    // Operators
+    Assign,
+    Plus,
+    Minus,
+    Bang,
+    Asterisk,
+    ForwardSlash,
+
+    LessThan,
+    GreaterThan,
+    Equal,
+    NotEqual,
+    // Delimiters
+    Comma,
+    Semicolon,
+
+    LParen,
+    RParen,
+    LBrace,
+    RBrace,
+    // Keywords
+    Function,
+    Let,
+    True,
+    False,
+    If,
+    Else,
+    Return,
+}
