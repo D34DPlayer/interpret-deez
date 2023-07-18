b7422734782f2999372437b4d38289559ac30beb
Carlos Ruiz Herrera <carlos.ruiz.herrera@rock.estate>
2023-07-19 00:36:47 +0200

    (feata) infix evaluation

--- src/evaluator/expressions.rs
+++ src/evaluator/expressions.rs
@@ -8,6 +8,7 @@ impl Evaluate for expr::Expression<'_> {
             Self::Integer(i) => i.eval(),
             Self::Boolean(b) => b.eval(),
             Self::Prefix(p) => p.eval(),
+            Self::Infix(i) => i.eval(),
             _ => Object::Null,
         }
     }
@@ -37,10 +38,41 @@ impl Evaluate for expr::Prefix<'_> {
             },
             expr::PrefixOp::Minus => match right {
                 Object::Integer(i) => Object::Integer(i * -1),
-                Object::Boolean(true) => Object::Integer(-1),
-                Object::Boolean(false) => Object::Integer(0),
-                Object::Null => Object::Null,
+                _ => Object::Null,
             },
         }
+    }
+}
+
+impl Evaluate for expr::Infix<'_> {
+    fn eval(&self) -> Object {
+        let left = self.left.eval();
+        let right = self.right.eval();
+        match (left, right) {
+            (Object::Integer(x), Object::Integer(y)) => evaluate_int_infix(&self.operator, x, y),
+            (Object::Boolean(x), Object::Boolean(y)) => evaluate_bool_infix(&self.operator, x, y),
+            _ => Object::Null,
+        }
+    }
+}
+
+fn evaluate_int_infix(op: &expr::InfixOp, x: i64, y: i64) -> Object {
+    match *op {
+        expr::InfixOp::Plus => Object::Integer(x + y),
+        expr::InfixOp::Minus => Object::Integer(x - y),
+        expr::InfixOp::Asterisk => Object::Integer(x * y),
+        expr::InfixOp::ForwardSlash => Object::Integer(x / y),
+        expr::InfixOp::Equal => Object::Boolean(x == y),
+        expr::InfixOp::GreaterThan => Object::Boolean(x > y),
+        expr::InfixOp::LessThan => Object::Boolean(x < y),
+        expr::InfixOp::NotEqual => Object::Boolean(x != y),
+    }
+}
+
+fn evaluate_bool_infix(op: &expr::InfixOp, x: bool, y: bool) -> Object {
+    match *op {
+        expr::InfixOp::Equal => Object::Boolean(x == y),
+        expr::InfixOp::NotEqual => Object::Boolean(x != y),
+        _ => Object::Null,
     }
 }
--- src/evaluator/test.rs
+++ src/evaluator/test.rs
@@ -66,12 +66,52 @@ fn test_eval() {
             expected: Object::Integer(12),
         },
         EvalTest {
-            input: "-true",
-            expected: Object::Integer(-1),
+            input: "60 + 9",
+            expected: Object::Integer(69),
         },
         EvalTest {
-            input: "-false",
+            input: "60 / 3",
+            expected: Object::Integer(20),
+        },
+        EvalTest {
+            input: "60 / 7",
+            expected: Object::Integer(8),
+        },
+        EvalTest {
+            input: "60 < 7",
+            expected: Object::Boolean(false),
+        },
+        EvalTest {
+            input: "60 > 7",
+            expected: Object::Boolean(true),
+        },
+        EvalTest {
+            input: "60 == 7",
+            expected: Object::Boolean(false),
+        },
+        EvalTest {
+            input: "60 == 60",
+            expected: Object::Boolean(true),
+        },
+        EvalTest {
+            input: "60 != 7",
+            expected: Object::Boolean(true),
+        },
+        EvalTest {
+            input: "60 != 60",
+            expected: Object::Boolean(false),
+        },
+        EvalTest {
+            input: "(5 + 10 * 2 + 15 / 3) * 2 + -10",
+            expected: Object::Integer(50),
+        },
+        EvalTest {
+            input: "-50 + 100 + -50",
             expected: Object::Integer(0),
+        },
+        EvalTest {
+            input: "(1 > 2) == false",
+            expected: Object::Boolean(true),
         },
     ];
 
